3ab9873d69fb5ba37f5bed1bbb62fd0aafa1d0cc
Maul <152008359+ulmaulana@users.noreply.github.com>
2025-12-05 20:09:53 +0700

    chore: route home page to new chat

--- packages/nextjs/app/page.tsx
+++ packages/nextjs/app/page.tsx
@@ -1,9 +1,9 @@
-import { ChatDemo } from "./chat";
+import { Chat } from "./chat";
 
 export default function Home() {
   return (
     <div className="flex flex-col gap-8 items-center sm:items-start w-full px-3 md:px-0">
-      <ChatDemo />
+      <Chat />
     </div>
   );
 }
